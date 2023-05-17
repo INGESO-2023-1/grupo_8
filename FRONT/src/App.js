05b9811c20c0d12642d7806f86877b785a2ab85e
Gabriel Vergara <gabriel.vergaraa@usm.cl>
2023-05-16 22:24:08 -0400

    Front Update

--- FRONT/src/App.js
+++ FRONT/src/App.js
@@ -1,13 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Inicio from './components/Inicio';
+import Home from './components/Home';
 import GetSim from './components/GetSim';
 
 function App() {
   return (
-    <div className="App">
-      <GetSim />
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Inicio />} /> 
+        <Route path="/Home" element={<Home />} /> 
+        <Route path="/getsim" element={<GetSim />}/>
+      </Routes>
+    </Router>
+
   );
 }
 
