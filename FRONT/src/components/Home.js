c1f1f8cec6d36b712daf7d502ab6017b166ae361
FelipeGuicharrousse <felipe.guicharrousse@usm.cl>
2023-05-17 23:39:02 -0400

    Actualizacion del Front-end Home

--- FRONT/src/components/Home.js
+++ FRONT/src/components/Home.js
@@ -1,37 +1,44 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import './Home.css'; // Archivo CSS personalizado para los colores
 
 function Home() {
   return (
-    <Container fluid>
-      <Row>
-        <Col md={4}>
+    <Container fluid style={{ height: '100vh', display: 'flex', flexDirection: 'column' }}>
+      <Row style={{ flex: 1 }}>
+        <Col md={4} className="custom-primary" style={{ height: '100%' }}>
           {/* Lista de chats */}
           <div className="p-3">
             <h3>Chats</h3>
             <ul className="list-group">
               {/* Renderiza los chats aquí */}
-              <li className="list-group-item">Chat 1</li>
-              <li className="list-group-item">Chat 2</li>
-              <li className="list-group-item">Chat 3</li>
+              <li className="list-group-item">Chat prueba 1</li>
+              <li className="list-group-item">Chat prueba 2</li>
+              <li className="list-group-item">Chat prueba 3</li>
             </ul>
           </div>
         </Col>
-        <Col md={8}>
+        <Col md={8} className="custom-secondary" style={{ height: '100%' }}>
           {/* Chat seleccionado */}
           <div className="p-3">
-            <h3>Chat seleccionado</h3>
+            <h3>Chat seleccionado / Nombre del contacto</h3>
             <div className="chat-messages">
               {/* Renderiza los mensajes del chat aquí */}
-              <div className="message">Mensaje 1</div>
-              <div className="message">Mensaje 2</div>
-              <div className="message">Mensaje 3</div>
+              <div className="message">Mensaje prueba 1</div>
+              <div className="message">Mensaje prueba 2</div>
+              <div className="message">Mensaje prueba 3</div>
             </div>
             {/* Casilla de texto para escribir mensajes */}
             <Form className="mt-3">
-              <Form.Control type="text" placeholder="Escribe tu mensaje" />
-              <Button variant="primary" className="mt-2">Enviar</Button>
+              <Row>
+                <Col xs={10}>
+                  <Form.Control type="text" placeholder="Escribe tu mensaje" />
+                </Col>
+                <Col xs={2}>
+                  <Button variant="primary">Enviar</Button>
+                </Col>
+              </Row>
             </Form>
           </div>
         </Col>
